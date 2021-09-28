fb3b95af7e8bec055a20e0f5bd0b50e81905c34f
Harsh Mehra <91560138+harshmehra1002@users.noreply.github.com>
2021-09-29 00:46:34 +0530

    Update Insert.c

--- LinkedList/Insert.c
+++ LinkedList/Insert.c
@@ -7,17 +7,50 @@ struct Node{
 void Traverse(struct Node *ptr){
     while(ptr!=NULL){
         printf("%d ",ptr->data);
-        printf("%d ",ptr->data);
         ptr=ptr->next;
     }
 }
+//case 1:
 struct Node *InsertAtFirst(struct Node *head,int element){
     struct Node *newhead=(struct Node*)malloc(sizeof(struct Node));
     newhead->data=1000;
     newhead->next=head;
     return newhead;
 }
-struct Node *InsertAtindex()
+//case 2:
+struct Node *InsertAtIndex(struct Node *head,int index,int element){
+    struct Node *ind=(struct Node*)malloc(sizeof(struct Node));
+    struct Node *p=head;
+    int i=0;
+    while(i!=(index-1)){
+         p=p->next;
+        i++;
+    }
+    ind->next=p->next;
+    p->next=ind;
+    ind->data=element;
+    return head;
+}
+//case 3:
+struct Node *InsertAtEnd(struct Node *head,int element){
+        struct Node *end=(struct Node*)malloc(sizeof(struct Node));
+        struct Node *p=head;
+        while(p->next!=NULL){
+            p=p->next;
+        }
+        p->next=end;
+        end->next=NULL;
+        end->data=element;
+        return head;
+}
+//case 4:
+void *InsertAfterNode(struct Node *prevNode,int element){
+    struct Node *ptr=(struct Node*)malloc(sizeof(struct Node));
+    ptr->data=element;
+    ptr->next=prevNode->next;
+    prevNode->next=ptr;
+
+}
 int main(){
     struct Node *head,*second,*third,*fourth;
     head=(struct Node*)malloc(sizeof(struct Node));
@@ -38,7 +71,10 @@ int main(){
     fourth->next=NULL;
 
     //Traverse(head);
-    head=InsertAtFirst(head,1000);
+   head=InsertAtFirst(head,1000);
+   head=InsertAtIndex(head,3,8000);
+   head=InsertAtEnd(head,4000);
+   InsertAfterNode(third,60);
     Traverse(head);
     
     return 0;
